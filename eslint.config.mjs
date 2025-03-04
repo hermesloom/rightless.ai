531cacde0c5525fde806f4bc93acd82d781a7f7e
henophilia <tech@henophilia.org>
2025-03-04 04:55:33 +0100

    fix: ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from "@next/eslint-plugin-next";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +16,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "@next": nextPlugin,
+      "@typescript-eslint": tseslint.plugin,
+      react: reactPlugin,
+      "react-hooks": hooksPlugin,
+      "jsx-a11y": jsxA11yPlugin,
+    },
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
